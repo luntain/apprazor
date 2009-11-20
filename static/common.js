6216bd8f6b919c153b26359f79c6ba1d8bddd1ed
luntain <kamil@resolversystems.com>
2009-11-20 11:00:28 +0000

    show tooltip for a point on hover
    The tooltip contains the revision of the measurement depicted
    by the point.

--- static/common.js
+++ static/common.js
@@ -13,8 +13,39 @@ function drawGraph(testData, title) {
     var revisions = map(function(res){return res[0];}, results).reverse();
     var baseline = best * (1+margin);
     var baselinePoints = [[revisions[0], baseline ], [revisions[revisions.length-1], baseline]]
-    var options = { }
+    var options = { grid: {hoverable: true} }
     var points = {data: zip(revisions, durations),
-                  points: {show: true}, lines: {show: true}}
-    $.plot($("#"+title), [{data: baselinePoints, color: "red"}, points], options)
+                  points: {show: true}, lines: {show: true},
+                  grid: {hoverable: true}}
+    var plot = $.plot($("#"+title), [{data: baselinePoints, color: "red"}, points], options)
+
+    function showTooltip(x, y, contents) {
+        $('<div id="tooltip">' + contents + '</div>').css( {
+            position: 'absolute',
+            display: 'none',
+            top: y + 5,
+            left: x + 5,
+            border: '1px solid #fdd',
+            padding: '2px',
+            'background-color': '#fee',
+            opacity: 0.80
+        }).appendTo("body").fadeIn(200);
+    }
+
+    var previousPoint = [0,0];
+    $("#"+title).bind("plothover", function (event, pos, item) {
+        if (item) {
+            if (previousPoint[0] != item.datapoint[0] || previousPoint[1] != item.datapoint[1]) {
+                previousPoint = item.datapoint;
+                $("#tooltip").remove();
+                var x = item.datapoint[0].toFixed(2),
+                    y = item.datapoint[1].toFixed(2);
+                showTooltip(Math.min(item.pageX, screen.width - 130), item.pageY,
+                            "rev "+ x.replace(/\..*/, ''));
+            }
+        } else {
+            $("#tooltip").remove();
+            previousPoint = [0,0];
+        }
+    });
 }
